bf3b71111e01e4702f60281d2d7ef02f881694bf
Onur Can Öğünç <ogunc606@gmail.com>
2024-12-09 13:41:38 +0300

    Add Color

--- app/src/main/java/com/decode/composenotes/ui/theme/Color.kt
+++ app/src/main/java/com/decode/composenotes/ui/theme/Color.kt
@@ -2,10 +2,87 @@ package com.decode.composenotes.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val primaryLight = Color(0xFFDCDADA)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFF505050)
+val onPrimaryContainerLight = Color(0xFFF0F0F0)
+val secondaryLight = Color(0xFF4A5B61)
+val onSecondaryLight = Color(0xFFFFFFFF)
+val secondaryContainerLight = Color(0xFF647379)
+val onSecondaryContainerLight = Color(0xFFE5E9EA)
+val surfaceLight = Color(0xFFF8F8F8)
+val onSurfaceLight = Color(0xFF212121)
+val surfaceVariantLight = Color(0xFFE2E2E2)
+val onSurfaceVariantLight = Color(0xFF4E4E4E)
+val addButtonLight = Color(0xFFF5B44A)
+val favButtonLight = Color(0xFF0074D9)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primaryDark = Color(0xFF1F1F1F)
+val onPrimaryDark = Color(0xFF212121)
+val primaryContainerDark = Color(0xFF383838)
+val onPrimaryContainerDark = Color(0xFF505050)
+val secondaryDark = Color(0xFF849298)
+val onSecondaryDark = Color(0xFFE8EBEC)
+val secondaryContainerDark = Color(0xFF3B4B52)
+val onSecondaryContainerDark = Color(0xFFC9D1D5)
+val surfaceDark = Color(0xFF121212)
+val onSurfaceDark = Color(0xFFE0E0E0)
+val surfaceVariantDark = Color(0xFF2D3538)
+val onSurfaceVariantDark = Color(0xFF9A9A9A)
+val addButtonDark = Color(0xFFFCAE2C)
+val favButtonDark = Color(0xFF00B8D9)
+
+sealed class ThemeColors(
+    val surface: Color,
+    val primary: Color,
+    val onPrimary: Color,
+    val primaryContainer: Color,
+    val onPrimaryContainer: Color,
+    val secondary: Color,
+    val onSecondary: Color,
+    val secondaryContainer: Color,
+    val onSecondaryContainer: Color,
+    val text: Color,
+    val onSurface: Color,
+    val surfaceVariant: Color,
+    val onSurfaceVariant: Color,
+    val addButton: Color,
+    val favButton: Color
+) {
+
+    object Night : ThemeColors( // Background color for dark mode
+        surface = surfaceLight,
+        primary = primaryLight,
+        onPrimary = onPrimaryDark,
+            primaryContainer = primaryContainerDark,
+        onPrimaryContainer = onPrimaryContainerDark,
+        secondary = secondaryDark,
+        onSecondary = onSecondaryDark,
+        secondaryContainer = secondaryContainerDark,
+        onSecondaryContainer = onSecondaryContainerDark,
+        text = onSurfaceDark,
+        onSurface = onSurfaceDark,
+        surfaceVariant = surfaceVariantDark,
+        onSurfaceVariant = onSurfaceVariantDark,
+        addButton = addButtonDark,
+        favButton = favButtonDark
+    )
+
+    object Day : ThemeColors(
+        surface = surfaceDark,
+        primary = primaryDark,
+        onPrimary = onPrimaryLight,
+        primaryContainer = primaryContainerLight,
+        onPrimaryContainer = onPrimaryContainerLight,
+        secondary = secondaryLight,
+        onSecondary = onSecondaryLight,
+        secondaryContainer = secondaryContainerLight,
+        onSecondaryContainer = onSecondaryContainerLight,
+        text = onSurfaceLight,
+        onSurface = onSurfaceLight,
+        surfaceVariant = surfaceVariantLight,
+        onSurfaceVariant = onSurfaceVariantLight,
+        addButton = addButtonLight,
+        favButton = favButtonLight
+    )
+}
--- app/src/main/java/com/decode/composenotes/ui/theme/Theme.kt
+++ app/src/main/java/com/decode/composenotes/ui/theme/Theme.kt
@@ -1,57 +1,72 @@
 package com.decode.composenotes.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    surface = ThemeColors.Night.surface,
+    primary = ThemeColors.Night.primary,
+    onPrimary = ThemeColors.Night.onPrimary,
+    primaryContainer = ThemeColors.Night.primaryContainer,
+    onPrimaryContainer = ThemeColors.Night.onPrimaryContainer,
+    secondary = ThemeColors.Night.secondary,
+    onSecondary = ThemeColors.Night.onSecondary,
+    secondaryContainer = ThemeColors.Night.secondaryContainer,
+    onSecondaryContainer = ThemeColors.Night.onSecondaryContainer,
+    background = Color(0xFF212121),
+    onBackground = ThemeColors.Night.onSurface,
+    surfaceVariant = ThemeColors.Night.surfaceVariant,
+    onSurfaceVariant = ThemeColors.Night.onSurfaceVariant,
+    onSurface = ThemeColors.Night.onSurface,
+    tertiary = ThemeColors.Night.favButton,
+    onTertiary = ThemeColors.Night.addButton,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    surface = ThemeColors.Day.surface,
+    primary = ThemeColors.Day.primary,
+    onPrimary = ThemeColors.Day.onPrimary,
+    primaryContainer = ThemeColors.Day.primaryContainer,
+    onPrimaryContainer = ThemeColors.Day.onPrimaryContainer,
+    secondary = ThemeColors.Day.secondary,
+    onSecondary = ThemeColors.Day.onSecondary,
+    secondaryContainer = ThemeColors.Day.secondaryContainer,
+    onSecondaryContainer = ThemeColors.Day.onSecondaryContainer,
+    background = Color(0xFFFFFFFF),
+    onBackground = ThemeColors.Day.onSurface,
+    surfaceVariant = ThemeColors.Day.surfaceVariant,
+    onSurfaceVariant = ThemeColors.Day.onSurfaceVariant,
+    onSurface = ThemeColors.Day.onSurface,
+    tertiary = ThemeColors.Day.favButton,
+    onTertiary = ThemeColors.Day.addButton,
 )
 
 @Composable
 fun ComposeNotesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val view = LocalView.current
+    val window = (view.context as Activity).window
+    val windowInsetsController = WindowCompat.getInsetsController(window, view)
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    windowInsetsController.isAppearanceLightStatusBars = !darkTheme
+
+    val colors = if (darkTheme) {
+       DarkColorScheme
+    } else {
+       LightColorScheme
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
         content = content
     )
